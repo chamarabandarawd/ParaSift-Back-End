8c382e7aff26ad74668f2b9aaf4d724576789828
chamarabandarawd <chamarabandarawd@gmail.com>
2023-09-04 08:16:27 +0530

    create app using express

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hi chamara")
+ const express=require("express");
+ const app=express();
+
+ const PORT=8001;
+
+ app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+ })
