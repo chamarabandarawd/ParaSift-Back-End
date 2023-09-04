706c835227d5a70b333497f32fa71591bba7fae5
chamarabandarawd <chamarabandarawd@gmail.com>
2023-09-04 10:41:12 +0530

    check connectivity using get req

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
  import express from "express";
  const app=express();
 
- const PORT=8001;
+ const PORT=5000;
 
  let post=[];
+
+ app.get("/posts",(req,res)=>{
+   res.send("hi")
+ })
 
  app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
