d51ad8faab1e3dd6838c407c724a4c414242dc98
chamarabandarawd <chamarabandarawd@gmail.com>
2023-09-04 08:20:08 +0530

    create local DB

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
- const express=require("express");
+ import express from "express";
  const app=express();
 
  const PORT=8001;
+
+ let post=[];
 
  app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
