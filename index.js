c55e30cfa410c7210a6b98fddbe5d8bd30245faf
chamarabandarawd <chamarabandarawd@gmail.com>
2023-09-05 18:48:36 +0530

    add config file to the project

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express, { json } from "express";
 import morgan from "morgan";
 import postRouter from "./api/posts.js";
+import { config } from "dotenv";
+
+config({path:"./config.env"})
+
+const PORT =process.env.PORT||3000;
 
 const app = express();
 
-const PORT = 5000;
 app.use(json());
 app.use(morgan("dev"));
 
